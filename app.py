b6de291164d9443cf87ba15f8fcbfa00903b4435
Piotr Wasilewski <piotr.wasilewski2@gmail.com>
2023-01-11 22:55:15 +0100

    Add login form Add .gitognore file Add styles

--- app.py
+++ app.py
@@ -1,1 +1,68 @@
+import os
 
+from cs50 import SQL
+from flask import Flask, flash, redirect, render_template, request, session
+from flask_session import Session
+# from tempfile import mkdtemp
+from werkzeug.security import check_password_hash, generate_password_hash
+from helpers import login_required
+# from datetime import datetime
+# from helpers import apology, login_required, lookup, usd, check_price, validate_password
+
+# Configure application
+app = Flask(__name__)
+
+# Ensure templates are auto-reloaded
+app.config["TEMPLATES_AUTO_RELOAD"] = True
+
+# Configure session to use filesystem (instead of signed cookies)
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+# Configure CS50 Library to use SQLite database
+# db = SQL("sqlite:///finance.db")
+
+
+@app.after_request
+def after_request(response):
+    """Ensure responses aren't cached"""
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
+
+
+@app.route("/")
+@login_required
+def index():
+    return render_template("index.html")
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    """Log user in"""
+    # Forget any user_id
+    session.clear()
+
+    # User reached route via POST (as by submitting a form via POST)
+    if request.method == "POST":
+        username = request.form.get("username")
+        print(username)
+        # Ensure username was submitted
+        if not username:
+            print(1)
+
+        # Ensure password was submitted
+        elif not request.form.get("password"):
+            return print(1)
+        print(request.form.get("password"))
+        # Remember which user has logged in
+        session["user_id"] = 5
+        session["username"] = username
+        # Redirect user to home page
+        return redirect("/")
+
+    # User reached route via GET (as by clicking a link or via redirect)
+    else:
+        return render_template("login.html")
