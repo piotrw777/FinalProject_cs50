f984588a9083430f41de52a04bba9099c74f27c3
Piotr Wasilewski <piotr.wasilewski2@gmail.com>
2023-01-13 09:20:52 +0100

    Improve login form Add JavaScript Add sass file

--- app.py
+++ app.py
@@ -20,7 +20,8 @@ app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
-# Configure CS50 Library to use SQLite database
+
+# Configure CS50 Library to use SQLite database  
 # db = SQL("sqlite:///finance.db")
 
 
@@ -44,7 +45,7 @@ def login():
     """Log user in"""
     # Forget any user_id
     session.clear()
-
+    print(3)
     # User reached route via POST (as by submitting a form via POST)
     if request.method == "POST":
         username = request.form.get("username")
@@ -66,3 +67,14 @@ def login():
     # User reached route via GET (as by clicking a link or via redirect)
     else:
         return render_template("login.html")
+
+
+@app.route("/logout")
+def logout():
+    """Log user out"""
+
+    # Forget any user_id
+    session.clear()
+
+    # Redirect user to login form
+    return redirect("/")
--- static/script.js
+++ static/script.js
@@ -1,3 +1,13 @@
-document.addEventListener('DOMContentLoaded', function() {
-    
-});
+function addEventListeners() {
+    const username = document.getElementById("username");
+    //const usernameValid = document.getElementById("username-valid");
+
+    //no tick at the beginning
+    usernameValid.style.display = "none";
+
+    username.addEventListener("input", function(){
+
+    });    
+}
+
+document.addEventListener('DOMContentLoaded', addEventListeners);
