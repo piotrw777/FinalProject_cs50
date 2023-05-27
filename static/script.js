f3e090e0ed9b1ccca2494ad3db96744d983720b2
Piotr Wasilewski <piotr.wasilewski2@gmail.com>
2023-05-27 13:17:33 +0200

    Fix closing alerts

--- static/script.js
+++ static/script.js
@@ -3,10 +3,11 @@ function addEventListeners() {
         function(button) {
             button.onclick = function() {
                 button.innerHTML = button.id;
-                window.location.assign('/download_pdf?filename=' + button.id);
-            }
-        }
-    )
-};
+                window.location.assign('/download_pdf?filename=' + button.id); }});
+
+    //closing alerts
+    let closeButton = document.querySelector('.close');
+    closeButton.addEventListener('click', function() {
+    document.querySelector('.alert').remove(); })};
 
 document.addEventListener('DOMContentLoaded', addEventListeners);
